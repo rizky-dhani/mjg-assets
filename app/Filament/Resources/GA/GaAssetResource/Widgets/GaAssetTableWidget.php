da8edbd5486727cc0df3580d795a1b5204c22c5b
Rizky Dhani <rizkydhani15@gmail.com>
2025-11-19 15:58:42 +0700

    feat: add left GA Doughnut and Table Widget

---
+++ app/Filament/Resources/GA/GaAssetResource/Widgets/GaAssetTableWidget.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Filament\Resources\GA\GaAssetResource\Widgets;
+
+use App\Filament\Resources\GA\GaAssetResource;
+use App\Models\GA\GaAsset;
+use App\Models\GA\GaAssetCategory;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget;
+
+class GaAssetTableWidget extends TableWidget
+{
+    protected static ?string $heading = 'GA Assets Summary';
+
+    // protected int | string | array $columnSpan = 'full';
+    protected static ?int $sort = 1;
+
+    public static function canView(): bool
+    {
+        // Only allow users with ITD division to see this widget
+        return auth()->user()?->division?->initial === 'GA';
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                GaAssetCategory::query()
+                    ->select('id', 'name')
+            )
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Category')
+                    ->weight('bold')
+                    ->formatStateUsing(function ($state) {
+                        return strtoupper($state);
+                    }),
+
+                Tables\Columns\TextColumn::make('available_count')
+                    ->label('Available')
+                    ->color('success')
+                    ->getStateUsing(function (GaAssetCategory $record) {
+                        $availableCount = GaAsset::where('asset_category_id', $record->id)
+                            ->whereNull('asset_user_id')
+                            ->count();
+
+                        return $availableCount;
+                    })
+                    ->url(fn ($record) => GaAssetResource::getUrl('index', [
+                        'tableFilters' => [
+                            'category_id' => [
+                                'value' => $record['id'],
+                            ],
+                            'asset_user_id' => [
+                                'available' => 'true',
+                                'in_use' => 'false',
+                            ],
+                        ],
+                    ]))
+                    ->weight('semibold'),
+
+                Tables\Columns\TextColumn::make('in_use_count')
+                    ->label('In Use')
+                    ->color('danger')
+                    ->getStateUsing(function (GaAssetCategory $record) {
+                        $inUseCount = GaAsset::where('asset_category_id', $record->id)
+                            ->whereNotNull('asset_user_id')
+                            ->count();
+
+                        return $inUseCount;
+                    })
+                    ->url(fn ($record) => GaAssetResource::getUrl('index', [
+                        'tableFilters' => [
+                            'category_id' => [
+                                'value' => $record['id'],
+                            ],
+                            'asset_user_id' => [
+                                'available' => 'false',
+                                'in_use' => 'true',
+                            ],
+                        ],
+                    ]))
+                    ->weight('semibold'),
+
+                Tables\Columns\TextColumn::make('total_count')
+                    ->label('Total')
+                    ->color('primary')
+                    ->getStateUsing(function (GaAssetCategory $record) {
+                        $totalCount = GaAsset::where('asset_category_id', $record->id)
+                            ->count();
+
+                        return $totalCount;
+                    })
+                    ->url(fn ($record) => GaAssetResource::getUrl('index', [
+                        'tableFilters' => [
+                            'category_id' => [
+                                'value' => $record['id'],
+                            ],
+                        ],
+                    ]))
+                    ->weight('semibold'),
+            ])
+            ->paginated(false)
+            ->striped();
+    }
+}
